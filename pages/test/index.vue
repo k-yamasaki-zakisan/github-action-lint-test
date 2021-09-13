75544b5beb3ba4baa97c4dc8cf3212b35d4790af
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-09-13 14:56:07 +0900

    add page

---
+++ pages/test/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{test}}</div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'nuxt-property-decorator'
+
+export default class extends Vue {
+  get test() {
+    return 'test'
+  }
+}
+</script>
