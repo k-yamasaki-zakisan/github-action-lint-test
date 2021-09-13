8535f27c88640cafc0feb73f55452aa0532924af
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-09-13 15:02:30 +0900

    fail test

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>{{test}}</div>
+  <div>
+    {{test}}
+    <p>{{test2}}</p>
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue } from 'nuxt-property-decorator'
 
-export default class extends Vue {
+export default class test extends Vue {
   get test() {
     return 'test'
+  }
+
+  get test2() {
+    return this.test == 'test'
   }
 }
 </script>
